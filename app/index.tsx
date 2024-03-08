1d5c6158aba146650b57f609273af4ee673a926c
mshivam019 <mshivam019@gmail.com>
2024-03-09 02:58:23 +0530

    add splash

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,25 @@
-import { ActivityIndicator, View } from "react-native";
+import { View,Dimensions } from "react-native";
+import LottieView from "lottie-react-native";
 
 const AnimatedSplashScreen = () => {
+	const { width, height } = Dimensions.get("window");
 	return (
-		<View style={{ flex: 1, justifyContent: "center" }}>
-			<ActivityIndicator size="large" color="#000" />
+		<View style={{ flex: 1,backgroundColor:'#9356ff' }}>
+			<LottieView
+				source={require("../assets/splash.json")}
+				autoPlay
+				loop={true}
+				speed={3}
+				style={{
+					position: "absolute",
+					top: 0,
+					left: 0,
+					bottom: 0,
+					right: 0,
+					width: width,
+					height: height,
+				}}
+			/>
 		</View>
 	);
 };

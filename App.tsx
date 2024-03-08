bca4c0c1ff620a5b4c9ced30bd1338dbdf67ed1e
mshivam019 <mshivam019@gmail.com>
2024-03-08 22:17:54 +0530

    update login screen

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { supabase } from "./lib/supabase";
 import Auth from "./components/Auth";
 import Account from "./components/Account";
-import { View } from "react-native";
+import { SafeAreaView } from "react-native";
 import { Session } from "@supabase/supabase-js";
 
 export default function App() {
@@ -20,12 +20,12 @@ export default function App() {
   }, []);
 
   return (
-    <View>
+    <SafeAreaView style={{flex:1}}>
       {session && session.user ? (
         <Account key={session.user.id} session={session} />
       ) : (
         <Auth />
       )}
-    </View>
+    </SafeAreaView>
   );
 }
--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,19 +1,45 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, View, Button, TextInput } from "react-native";
+import {
+	Alert,
+	View,
+	TextInput,
+	Text,
+	Dimensions,
+	Pressable,
+	StyleSheet,
+	KeyboardAvoidingView,
+	Keyboard,
+} from "react-native";
+import Svg, { Image, Ellipse, ClipPath } from "react-native-svg";
+import styles from "../styles/loginStyles";
+import Animated, {
+	useSharedValue,
+	useAnimatedStyle,
+	interpolate,
+	withTiming,
+	withDelay,
+	runOnJS,
+	withSequence,
+	withSpring,
+} from "react-native-reanimated";
 import { supabase } from "../lib/supabase";
 import {
 	GoogleSignin,
 	GoogleSigninButton,
 	statusCodes,
 } from "@react-native-google-signin/google-signin";
+import LottieView from "lottie-react-native";
 
 export default function Auth() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [loading, setLoading] = useState(false);
+	const { height, width } = Dimensions.get("window");
+	const imagePosition = useSharedValue(1);
+	const formButtonScale = useSharedValue(1);
+	const [isRegistering, setIsRegistering] = useState(false);
 
 	GoogleSignin.configure({
-		scopes: ["https://www.googleapis.com/auth/drive.readonly"],
 		webClientId:
 			"537241639551-pbpfuoe8kh9g22uc66pomohe5sho1545.apps.googleusercontent.com",
 		offlineAccess: true,
@@ -46,97 +72,229 @@ export default function Auth() {
 		setLoading(false);
 	}
 
+	const imageAnimatedStyle = useAnimatedStyle(() => {
+		const interpolation = interpolate(
+			imagePosition.value,
+			[0, 1],
+			[-height / 2, 0]
+		);
+		return {
+			transform: [
+				{ translateY: withTiming(interpolation, { duration: 1000 }) },
+			],
+		};
+	});
+
+	const buttonsAnimatedStyle = useAnimatedStyle(() => {
+		const interpolation = interpolate(
+			imagePosition.value,
+			[0, 1],
+			[250, 0]
+		);
+		return {
+			opacity: withTiming(imagePosition.value, { duration: 500 }),
+			transform: [
+				{ translateY: withTiming(interpolation, { duration: 1000 }) },
+			],
+		};
+	});
+
+	const closeButtonContainerStyle = useAnimatedStyle(() => {
+		const interpolation = interpolate(
+			imagePosition.value,
+			[0, 1],
+			[180, 360]
+		);
+		return {
+			opacity: withTiming(imagePosition.value === 1 ? 0 : 1, {
+				duration: 800,
+			}),
+			transform: [
+				{
+					rotate: withTiming(interpolation + "deg", {
+						duration: 1000,
+					}),
+				},
+			],
+		};
+	});
+
+	const formAnimatedStyle = useAnimatedStyle(() => {
+		return {
+			opacity:
+				imagePosition.value === 0
+					? withDelay(400, withTiming(1, { duration: 800 }))
+					: withTiming(0, { duration: 300 }),
+		};
+	});
+
+	const formButtonAnimatedStyle = useAnimatedStyle(() => {
+		return {
+			transform: [{ scale: formButtonScale.value }],
+		};
+	});
+
+	const loginHandler = () => {
+		imagePosition.value = 0;
+		if (isRegistering) {
+			runOnJS(setIsRegistering)(false);
+		}
+	};
+
+	const registerHandler = () => {
+		imagePosition.value = 0;
+		if (!isRegistering) {
+			runOnJS(setIsRegistering)(true);
+		}
+	};
+
+	const buttonHandler = () => {
+		if (loading) return;
+		formButtonScale.value = withSequence(withSpring(1.5), withSpring(1));
+		if (!email || !password) {
+			Alert.alert("Please fill in all fields!");
+			return;
+		}
+		if (isRegistering) {
+			signUpWithEmail();
+		} else {
+			signInWithEmail();
+		}
+	};
 	return (
-		<View style={styles.container}>
-			<View style={[styles.verticallySpaced, styles.mt20]}>
-				<TextInput
-					onChangeText={(text) => setEmail(text)}
-					value={email}
-					placeholder="email@address.com"
-					autoCapitalize={"none"}
-					style={styles.input}
-				/>
-			</View>
-			<View style={styles.verticallySpaced}>
-				<TextInput
-					onChangeText={(text) => setPassword(text)}
-					value={password}
-					secureTextEntry={true}
-					placeholder="Password"
-					autoCapitalize={"none"}
-					style={styles.input}
-				/>
-			</View>
-			<View style={[styles.verticallySpaced, styles.mt20]}>
-				<Button
-					title="Sign in"
-					disabled={loading}
-					onPress={() => signInWithEmail()}
-				/>
-			</View>
-			<View style={styles.verticallySpaced}>
-				<Button
-					title="Sign up"
-					disabled={loading}
-					onPress={() => signUpWithEmail()}
-				/>
-			</View>
-			<GoogleSigninButton
-				size={GoogleSigninButton.Size.Wide}
-				color={GoogleSigninButton.Color.Light}
-				onPress={async () => {
-					try {
-						await GoogleSignin.hasPlayServices();
-						const userInfo = await GoogleSignin.signIn();
-						if (userInfo.idToken) {
-							const { data, error } =
-								await supabase.auth.signInWithIdToken({
-									provider: "google",
-									token: userInfo.idToken,
-								});
-							console.log(error, data);
-						} else {
-							throw new Error("no ID token present!");
-						}
-					} catch (error: any) {
-						console.error(error);
-						if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-							// user cancelled the login flow
-						} else if (error.code === statusCodes.IN_PROGRESS) {
-							// operation (e.g. sign in) is in progress already
-						} else if (
-							error.code ===
-							statusCodes.PLAY_SERVICES_NOT_AVAILABLE
-						) {
-							// play services not available or outdated
-						} else {
-							// some other error happened
-						}
-					}
-				}}
-			/>
-		</View>
+		<Animated.View style={styles.container}>
+			<Animated.View
+				style={[StyleSheet.absoluteFill, imageAnimatedStyle]}
+			>
+				<Svg height={height + 100} width={width + 100}>
+					<LottieView
+						autoPlay
+						style={{
+							position: "absolute",
+							width: 300,
+							height: 300,
+							zIndex: 1,
+							top: height / 12,
+							right: width / 5,
+						}}
+						source={require("../assets/hello.json")}
+					/>
+					<ClipPath id="clipPathId">
+						<Ellipse cx={width / 2} rx={height} ry={height + 100} />
+					</ClipPath>
+					<Image
+						href={require("../assets/loginBackground.png")}
+						width={width + 100}
+						height={height + 100}
+						preserveAspectRatio="xMidYMid slice"
+						clipPath="url(#clipPathId)"
+					/>
+				</Svg>
+				<Pressable
+					onPress={() => {
+						imagePosition.value = 1;
+						Keyboard.dismiss();
+					}}
+				>
+					<Animated.View
+						style={[
+							styles.closeButtonContainer,
+							closeButtonContainerStyle,
+						]}
+					>
+						<Text style={styles.buttonText}>X</Text>
+					</Animated.View>
+				</Pressable>
+			</Animated.View>
+			<KeyboardAvoidingView behavior="height">
+				<View style={styles.bottomContainer}>
+					<Animated.View style={buttonsAnimatedStyle}>
+						<Pressable style={styles.button} onPress={loginHandler}>
+							<Text style={styles.buttonText}>LOG IN</Text>
+						</Pressable>
+					</Animated.View>
+					<Animated.View style={buttonsAnimatedStyle}>
+						<Pressable
+							style={styles.button}
+							onPress={registerHandler}
+						>
+							<Text style={styles.buttonText}>REGISTER</Text>
+						</Pressable>
+					</Animated.View>
+					<Animated.View
+						style={[styles.formInputContainer, formAnimatedStyle]}
+					>
+						<TextInput
+							placeholder="Email"
+							placeholderTextColor="black"
+							onChangeText={(text) => setEmail(text)}
+							value={email}
+							style={styles.textInput}
+							autoCapitalize={"none"}
+						/>
+						<TextInput
+							placeholder="Password"
+							placeholderTextColor="black"
+							style={styles.textInput}
+							onChangeText={(text) => setPassword(text)}
+							value={password}
+							secureTextEntry={true}
+							autoCapitalize="none"
+						/>
+						<Animated.View
+							style={[styles.formButton, formButtonAnimatedStyle]}
+						>
+							<Pressable onPress={() => buttonHandler()}>
+								<Text style={styles.actionButtonText}>
+									{isRegistering ? "REGISTER" : "LOG IN"}
+								</Text>
+							</Pressable>
+						</Animated.View>
+						<GoogleSigninButton
+							size={GoogleSigninButton.Size.Wide}
+							color={GoogleSigninButton.Color.Light}
+							onPress={async () => {
+								try {
+									await GoogleSignin.hasPlayServices();
+									const userInfo =
+										await GoogleSignin.signIn();
+									if (userInfo.idToken) {
+										const { data, error } =
+											await supabase.auth.signInWithIdToken(
+												{
+													provider: "google",
+													token: userInfo.idToken,
+												}
+											);
+										console.log(error, data);
+									} else {
+										throw new Error("no ID token present!");
+									}
+								} catch (error: any) {
+									console.error(error);
+									if (
+										error.code ===
+										statusCodes.SIGN_IN_CANCELLED
+									) {
+										// user cancelled the login flow
+									} else if (
+										error.code === statusCodes.IN_PROGRESS
+									) {
+										// operation (e.g. sign in) is in progress already
+									} else if (
+										error.code ===
+										statusCodes.PLAY_SERVICES_NOT_AVAILABLE
+									) {
+										// play services not available or outdated
+									} else {
+										// some other error happened
+									}
+								}
+							}}
+						/>
+					</Animated.View>
+				</View>
+			</KeyboardAvoidingView>
+		</Animated.View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		marginTop: 40,
-		padding: 12,
-    alignItems: "center",
-	},
-	verticallySpaced: {
-		paddingTop: 4,
-		paddingBottom: 4,
-		alignSelf: "stretch",
-	},
-	mt20: {
-		marginTop: 20,
-	},
-	input: {
-		height: 40,
-		margin: 12,
-		borderWidth: 1,
-		padding: 10,
-	},
-});

eb3907ad25e2f801bb6d6ede769d5f41baad833e
mshivam019 <mshivam019@gmail.com>
2024-03-08 17:06:16 +0530

    Add new Android resources

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,92 +1,95 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View, Button, TextInput } from "react-native";
 import { supabase } from "../lib/supabase";
-import { Button, Input } from "react-native-elements";
 
 export default function Auth() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [loading, setLoading] = useState(false);
 
-  async function signInWithEmail() {
-    setLoading(true);
-    const { error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
+	async function signInWithEmail() {
+		setLoading(true);
+		const { error } = await supabase.auth.signInWithPassword({
+			email: email,
+			password: password,
+		});
 
-    if (error) Alert.alert(error.message);
-    setLoading(false);
-  }
+		if (error) Alert.alert(error.message);
+		setLoading(false);
+	}
 
-  async function signUpWithEmail() {
-    setLoading(true);
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
+	async function signUpWithEmail() {
+		setLoading(true);
+		const {
+			data: { session },
+			error,
+		} = await supabase.auth.signUp({
+			email: email,
+			password: password,
+		});
 
-    if (error) Alert.alert(error.message);
-    if (!session)
-      Alert.alert("Please check your inbox for email verification!");
-    setLoading(false);
-  }
+		if (error) Alert.alert(error.message);
+		if (!session)
+			Alert.alert("Please check your inbox for email verification!");
+		setLoading(false);
+	}
 
-  return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: "font-awesome", name: "envelope" }}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize={"none"}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: "font-awesome", name: "lock" }}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={"none"}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title="Sign in"
-          disabled={loading}
-          onPress={() => signInWithEmail()}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button
-          title="Sign up"
-          disabled={loading}
-          onPress={() => signUpWithEmail()}
-        />
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={[styles.verticallySpaced, styles.mt20]}>
+				<TextInput
+					onChangeText={(text) => setEmail(text)}
+					value={email}
+					placeholder="email@address.com"
+					autoCapitalize={"none"}
+					style={styles.input}
+				/>
+			</View>
+			<View style={styles.verticallySpaced}>
+				<TextInput
+					onChangeText={(text) => setPassword(text)}
+					value={password}
+					secureTextEntry={true}
+					placeholder="Password"
+					autoCapitalize={"none"}
+					style={styles.input}
+				/>
+			</View>
+			<View style={[styles.verticallySpaced, styles.mt20]}>
+				<Button
+					title="Sign in"
+					disabled={loading}
+					onPress={() => signInWithEmail()}
+				/>
+			</View>
+			<View style={styles.verticallySpaced}>
+				<Button
+					title="Sign up"
+					disabled={loading}
+					onPress={() => signUpWithEmail()}
+				/>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: "stretch",
-  },
-  mt20: {
-    marginTop: 20,
-  },
+	container: {
+		marginTop: 40,
+		padding: 12,
+	},
+	verticallySpaced: {
+		paddingTop: 4,
+		paddingBottom: 4,
+		alignSelf: "stretch",
+	},
+	mt20: {
+		marginTop: 20,
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
 });
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,31 @@
 import "react-native-url-polyfill/auto";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { MMKV } from "react-native-mmkv";
+
+const storage = new MMKV();
+
+const zustandStorage = {
+	setItem: (name: string, value: any) => {
+		return storage.set(name, value);
+	},
+	getItem: (name: string) => {
+		const value = storage.getString(name);
+		return value ?? null;
+	},
+	removeItem: (name: string) => {
+		return storage.delete(name);
+	},
+};
 
 const supabaseUrl = "https://iawrnpgcvsjmwdgpkaav.supabase.co";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlhd3JucGdjdnNqbXdkZ3BrYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4ODYxMzMsImV4cCI6MjAyNTQ2MjEzM30.9xk7DrUOAXrxHZN3ctIV32uisn4amMEIRJoNrv6uGYA";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlhd3JucGdjdnNqbXdkZ3BrYWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4ODYxMzMsImV4cCI6MjAyNTQ2MjEzM30.9xk7DrUOAXrxHZN3ctIV32uisn4amMEIRJoNrv6uGYA";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
+	auth: {
+		storage: zustandStorage,
+		autoRefreshToken: true,
+		persistSession: true,
+		detectSessionInUrl: false,
+	},
 });
